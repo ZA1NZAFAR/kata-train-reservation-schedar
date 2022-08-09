49ae9057b1f827fb31345ec1ebc87654540987ae
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-08-09 11:40:38 +0200

    Add end-to-end test for `ping()`

--- ticket_office/java/src/test/java/fr/arrolla/trainreservation/TrainReservationApplicationTests.java
+++ ticket_office/java/src/test/java/fr/arrolla/trainreservation/TrainReservationApplicationTests.java
@@ -1,13 +1,33 @@
 package fr.arrolla.trainreservation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TrainReservationApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void canCallPing() throws Exception {
+		var query = get("http://127.0.0.1:8083/ping");
+		var statusOk = status().isOk();
+		var response = mockMvc.perform(query).andExpect(statusOk).andReturn().getResponse();
+		var body = response.getContentAsString();
+		assertEquals("pong", body);
 	}
 
 }
