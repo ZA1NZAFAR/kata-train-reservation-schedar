13c888fd2aaa273a284d6539a60d4ae1c48db7b7
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-12-29 13:51:48 +0100

    Implement TrainDataParser

---
+++ ticket_office/cs/TicketOffice.Tests/Infra/TrainDataParserTests.cs
@@ -0,0 +1,34 @@
+﻿using TicketOffice.Infra;
+
+namespace TicketOffice.Tests.Infra;
+
+public class TrainDataParserTests
+{
+    [Test]
+    public void parsing_train_data()
+    {
+        string trainDataJson = """    
+        {
+           "seats": {
+              "1A": {
+                "booking_reference": "",
+                  "coach": "A",
+                  "seat_number": "1"
+              },
+              "1B": {
+                "booking_reference": "abc123",
+                  "coach": "B",
+                  "seat_number": "1"
+              }
+            }
+        }
+        """;
+
+        var train = TrainDataParser.Parse(trainDataJson);
+        var seat1 = train.GetSeat("1A");
+        Assert.That(seat1.IsFree());
+        var seat2 = train.GetSeat("1B");
+        Assert.That(seat2.BookingReference!, Is.EqualTo("abc123"));
+    }
+
+}
---
+++ ticket_office/cs/TicketOffice/Infra/TrainDataParser.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using TicketOffice.Domain;
+
+namespace TicketOffice.Infra;
+
+public class TrainDataParser
+{
+    public static Train Parse(string json)
+    {
+        var train = new Train();
+
+        var data = JObject.Parse(json);
+        var jsonSeats = data["seats"].Values();
+        foreach (var jsonSeat in jsonSeats)
+        {
+            var seatNumber = jsonSeat["seat_number"].Value<string>();
+            var coachId = jsonSeat["coach"].Value<string>();
+            var seatId = seatNumber + coachId;
+            var seat = new Seat(seatId);
+            var jsonBookingReference = jsonSeat["booking_reference"].Value<string>();
+            if (!jsonBookingReference.Equals(""))
+            {
+                {
+                    seat.Book(jsonBookingReference);
+                }
+            }
+            train.Add(seat);
+
+        }
+        return train;
+    }
+}
