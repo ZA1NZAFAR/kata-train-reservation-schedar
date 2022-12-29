287ad887df343e19bbce31850d2f22b053990a25
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-12-29 13:37:12 +0100

    Finally we can unit test the controller
    ...
    
    But it fails with a Json Error

---
+++ ticket_office/cs/TicketOffice.Tests/TicketOfficeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using TicketOffice.Controllers;
+
+namespace TicketOffice.Tests;
+
+public class TicketOfficeControllerTests
+{
+    [Test]
+    public async Task reserve_four_seats_from_empty_train()
+    {
+        const string trainId = "express_2000";
+        var train = Helpers.MakeEmptyTrain();
+        var fakeRestClient = new FakeRestClient(train);
+        var bookingRequest = new BookingRequest(trainId, 4);
+        var controller = new TicketOfficeController(fakeRestClient);
+
+        var response = await controller.Reserve(bookingRequest);
+    }
+
+}
