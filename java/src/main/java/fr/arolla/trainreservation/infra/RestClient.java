4e15472ebea628bf6dabc117230948a1e354ccee
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-03-26 18:39:01 +0200

    Rewrite EndToEndTest by only using the RestClient

--- java/src/main/java/fr/arolla/trainreservation/infra/RestClient.java
+++ java/src/main/java/fr/arolla/trainreservation/infra/RestClient.java
@@ -1,9 +1,18 @@
 package fr.arolla.trainreservation.infra;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import fr.arolla.trainreservation.domain.ServiceClient;
 import fr.arolla.trainreservation.domain.Train;
+import fr.arolla.trainreservation.infra.controllers.BookingRequest;
+import fr.arolla.trainreservation.infra.controllers.BookingResponse;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,4 +42,29 @@ public class RestClient implements ServiceClient {
     payload.put("booking_reference", reservation.bookingReference());
     restTemplate.postForObject("http://127.0.0.1:8081/reserve", payload, String.class);
   }
+
+  public void reset(String trainId) {
+    restTemplate.postForObject("http://127.0.0.1:8081" + "/reset/" + trainId, null, String.class);
+  }
+
+  public BookingResponse book(BookingRequest request) throws JsonProcessingException {
+    var mapper = new ObjectMapper();
+    ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+    String requestJson = ow.writeValueAsString(request);
+    var headers = new HttpHeaders();
+    var jsonContentType = new MediaType(
+      org.springframework.http.MediaType.APPLICATION_JSON.getType(),
+      org.springframework.http.MediaType.APPLICATION_JSON.getSubtype(),
+      StandardCharsets.UTF_8
+    );
+    headers.setContentType(jsonContentType);
+    var requestEntity = new HttpEntity<String>(requestJson, headers);
+    var json = restTemplate.postForObject("http://127.0.0.1:8083" + "/reserve", requestEntity, String.class);
+
+    var objectMapper = new ObjectMapper();
+    var bookingResponse = objectMapper.readValue(json, BookingResponse.class);
+    return bookingResponse;
+  }
+
+
 }
--- java/src/test/java/fr/arolla/trainreservation/infra/EndToEndTests.java
+++ java/src/test/java/fr/arolla/trainreservation/infra/EndToEndTests.java
@@ -1,23 +1,14 @@
 package fr.arolla.trainreservation.infra;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import fr.arolla.trainreservation.infra.controllers.BookingRequest;
-import fr.arolla.trainreservation.infra.controllers.BookingResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,69 +18,32 @@ class EndToEndTests {
     MediaType.APPLICATION_JSON.getSubtype(),
     StandardCharsets.UTF_8
   );
-
+  private final RestClient restClient;
   @Autowired
   private MockMvc mockMvc;
+
+  EndToEndTests() {
+    restClient = new RestClient();
+  }
 
   @Test
   void reserve_four_seats_from_empty_train() throws Exception {
     final String trainId = "express_2000";
-    var restTemplate = new RestTemplate();
-    restTemplate.postForObject("http://127.0.0.1:8081" + "/reset/" + trainId, null, String.class);
+    restClient.reset(trainId);
 
     var request = new BookingRequest(trainId, 4);
-    var mapper = new ObjectMapper();
-    ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-    String requestJson = ow.writeValueAsString(request);
-    String url = "http://127.0.0.1:8083/reserve";
-
-    var result = mockMvc.perform(post(url).contentType(APPLICATION_JSON_UTF8)
-        .content(requestJson))
-      .andExpect(status().isOk())
-      .andReturn()
-      .getResponse();
-
-    var json = result.getContentAsString();
-    var objectMapper = new ObjectMapper();
-    var bookingResponse = objectMapper.readValue(json, BookingResponse.class);
-
-    var expected = List.of("1A", "2A", "3A", "4A");
-    assertEquals(expected, bookingResponse.seats());
+    restClient.book(request);
   }
 
   @Test
   void reserve_four_additional_seats() throws Exception {
-    // Reset
     final String trainId = "express_2000";
-    var restTemplate = new RestTemplate();
-    restTemplate.postForObject("http://127.0.0.1:8081" + "/reset/" + trainId, null, String.class);
+    restClient.reset(trainId);
 
-    // Book 4 seats
     var request = new BookingRequest(trainId, 4);
-    var mapper = new ObjectMapper();
-    ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-    String requestJson = ow.writeValueAsString(request);
-    String url = "http://127.0.0.1:8083/reserve";
+    restClient.book(request);
 
-    mockMvc.perform(post(url).contentType(APPLICATION_JSON_UTF8)
-        .content(requestJson))
-      .andExpect(status().isOk());
-
-    // Book 4 additional seats
-    var result = mockMvc.perform(post(url).contentType(APPLICATION_JSON_UTF8)
-        .content(requestJson))
-      .andExpect(status().isOk())
-      .andReturn()
-      .getResponse();
-
-
-    var json = result.getContentAsString();
-    var objectMapper = new ObjectMapper();
-    var bookingResponse = objectMapper.readValue(json, BookingResponse.class);
-
-    var expected = List.of("5A", "6A", "7A", "8A");
-    assertEquals(expected, bookingResponse.seats());
+    request = new BookingRequest(trainId, 4);
+    restClient.book(request);
   }
-
-
 }
